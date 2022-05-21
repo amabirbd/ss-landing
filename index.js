5a4a7b3f31a4d4565fbdd8783d2c1293783b84b9
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-05-21 16:25:18 +0600

    chore: commented out the firebase codes

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, getDocs, collection } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { collection, getDocs, getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.MEASUREMENT_ID,
+// };
 
-initializeApp(firebaseConfig);
+// initializeApp(firebaseConfig);
 
-const db = getFirestore();
+// const db = getFirestore();
 
-const colRef = collection(db, "messages");
+// const colRef = collection(db, "messages");
 
-getDocs(colRef).then((snapshot) => {
-  let messages = [];
-  snapshot.docs.forEach((doc) => {
-    messages.push({ ...doc.data(), id: doc.id });
-  });
-  console.log(messages);
-});
+// getDocs(colRef).then((snapshot) => {
+//   let messages = [];
+//   snapshot.docs.forEach((doc) => {
+//     messages.push({ ...doc.data(), id: doc.id });
+//   });
+//   console.log(messages);
+// });
+
+console.log('hello')
